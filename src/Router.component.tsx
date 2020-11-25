f3860d823aab433d78a360190d885b6cb149c04d
Damien Jakob <damien.jakob@cpnv.ch>
2020-11-25 10:58:47 +0100

    Add route and base component

--- src/Router.component.tsx
+++ src/Router.component.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
 import {HomeComponent} from "./home/Home.component";
+import {ClientsIndexComponent} from "./clients/Clients.component";
 
 export function RouterComponent() {
     return (
         <Switch>
+            <Route path="/clients">
+                <ClientsIndexComponent/>
+            </Route>
             <Route path="/">
                 <HomeComponent/>
             </Route>
---
+++ src/clients/Clients.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function ClientsIndexComponent() {
+	return (
+		<div>
+			<h1>Clients List</h1>
+		</div>
+	)
+}

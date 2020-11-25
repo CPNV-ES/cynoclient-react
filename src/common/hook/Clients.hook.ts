1a22c56669fe58a45c214e83ef98987905c860d9
Ansermoz Gwenaël <25158748+ansermgw@users.noreply.github.com>
2020-11-25 12:04:58 +0100

    fetch client by id

--- src/common/hook/Clients.hook.ts
+++ src/common/hook/Clients.hook.ts
@@ -1,10 +1,28 @@
 import {ClientsRepository} from "../repository/Clients.repository";
-import {useQuery} from "react-query";
+import {useQuery, useQueryCache} from "react-query";
 import {Client} from "../resource/Client.resource";
 import {List} from "immutable";
 
 const CLIENT_CACHE_KEY = "clients"
 
 export function useClients() {
-    return useQuery<List<Client>>(CLIENT_CACHE_KEY, () => ClientsRepository.getClients())
+    const cache = useQueryCache();
+
+    return useQuery<List<Client>>(
+        CLIENT_CACHE_KEY,
+        () => ClientsRepository.getClients(),
+        {
+            onSuccess: (clients => {
+                //Also add each client in the cache by their id to avoid refetching them when using useClient
+                clients.forEach(client => {
+                    cache.setQueryData([CLIENT_CACHE_KEY, client.id], client)
+                })
+            })
+        }
+    )
+}
+
+export function useClient(id: number) {
+    return useQuery<Client | null>([CLIENT_CACHE_KEY, id],
+        () => ClientsRepository.getClient(id))
 }

f9699ec46ae862f2e5cc423aa42ff259aeec847c
Damien Jakob <damien.jakob@cpnv.ch>
2020-11-26 14:55:01 +0100

    Add Dog page

--- src/Router.component.tsx
+++ src/Router.component.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 import {HomeComponent} from "./home/Home.component";
 import {ClientsIndexComponent} from "./clients/ClientsIndex.component";
+import {DogsIndexComponent} from "./dogs/index/DogsIndex.component";
 
 export function RouterComponent() {
     return (
         <Switch>
             <Route path="/clients">
                 <ClientsIndexComponent/>
+            </Route>
+            <Route path="/dogs">
+                <DogsIndexComponent/>
             </Route>
             <Route path="/">
                 <HomeComponent/>
---
+++ src/dogs/index/DogsIndex.component.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function DogsIndexComponent() {
+	return (
+		<div>
+			<h1>Chiens</h1>
+		</div>
+	);
+}

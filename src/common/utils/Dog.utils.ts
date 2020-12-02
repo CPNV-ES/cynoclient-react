f4f41ef149f5f9a8f9ab0f1b81c351c7005b3fc6
Damien Jakob <damien.jakob@cpnv.ch>
2020-12-02 14:21:10 +0100

    Add dog age

--- src/common/utils/Dog.utils.ts
+++ src/common/utils/Dog.utils.ts
@@ -1,9 +1,13 @@
 import {Dog} from "../resource/Dog.resource";
+import moment from "moment";
 
 export function displayDogSex(dog: Dog): String {
-	return dog.isFemale ? "Femelle" : "Mâle";
+    return dog.isFemale ? "Femelle" : "Mâle";
 }
 
-export function dogAge(dog: Dog): number {
-	return 0;
+export function dogAge(dog: Dog): number | null {
+    if (dog.isDead) {
+        return null;
+    }
+    return moment().diff(dog.birthdate, "years");
 }

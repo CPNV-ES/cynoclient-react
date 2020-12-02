b629271508bced62839259d1d70a5874b99c8c83
Damien Jakob <damien.jakob@cpnv.ch>
2020-12-02 14:00:43 +0100

    Add all dog data in view

--- src/common/utils/Dog.utils.ts
+++ src/common/utils/Dog.utils.ts
@@ -3,3 +3,7 @@ import {Dog} from "../resource/Dog.resource";
 export function displayDogSex(dog: Dog): String {
 	return dog.isFemale ? "Femelle" : "Mâle";
 }
+
+export function dogAge(dog: Dog): number {
+	return 0;
+}
--- src/dogs/index/DogsTable.component.tsx
+++ src/dogs/index/DogsTable.component.tsx
@@ -5,36 +5,43 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableBody from "@material-ui/core/TableBody";
 import {Dog} from "../../common/resource/Dog.resource";
-import {displayDogSex} from "../../common/utils/Dog.utils";
+import {displayDogSex, dogAge} from "../../common/utils/Dog.utils";
+import {displayBool} from "../../common/utils/Data.utils";
 
 export function DogRow(dog: Dog) {
-	return (
-		<TableRow>
-			<TableCell>{dog.noun}</TableCell>
-			<TableCell>{displayDogSex(dog)}</TableCell>
-		</TableRow>
-	);
+    return (
+        <TableRow>
+            <TableCell>{dog.noun}</TableCell>
+            <TableCell>{displayDogSex(dog)}</TableCell>
+            <TableCell>{displayBool(dog.isDead)}</TableCell>
+            <TableCell>{dogAge(dog)}</TableCell>
+            <TableCell>{dog.breed}</TableCell>
+            <TableCell>{dog.crossbreed}</TableCell>
+            <TableCell>{dog.id_client}</TableCell>
+        </TableRow>
+    );
 }
 
 export function DogsTable() {
-	const {data: dogs} = useDogs();
-	return (
-		<TableContainer>
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell>Nom</TableCell>
-						<TableCell>Sexe</TableCell>
-						<TableCell>Mort ?</TableCell>
-						<TableCell>Espèce</TableCell>
-						<TableCell>Croisement</TableCell>
-						<TableCell>Propiétaire</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{dogs?.map((dog) => DogRow(dog))}
-				</TableBody>
-			</Table>
-		</TableContainer>
-	);
+    const {data: dogs} = useDogs();
+    return (
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Nom</TableCell>
+                        <TableCell>Sexe</TableCell>
+                        <TableCell>Mort ?</TableCell>
+                        <TableCell>Age</TableCell>
+                        <TableCell>Espèce</TableCell>
+                        <TableCell>Croisement</TableCell>
+                        <TableCell>Propiétaire</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {dogs?.map((dog) => DogRow(dog))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }

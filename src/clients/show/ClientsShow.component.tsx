b76749c71ad2f831e8a8745651e8e64596ad5906
Ansermoz Gwenaël <25158748+ansermgw@users.noreply.github.com>
2020-12-02 10:19:51 +0100

    add paper wrapper to client detail

--- src/clients/show/ClientsShow.component.tsx
+++ src/clients/show/ClientsShow.component.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {Redirect, useHistory, useParams} from "react-router-dom";
 import {useClient} from "../../common/hook/Clients.hook";
-import {Button, createStyles, Grid, Theme} from "@material-ui/core";
+import {Button, createStyles, Grid, Paper, Theme} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import {displayClientSex} from "../../common/utils/Client.utils";
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export function ClientsShowComponent() {
     const styles = useStyles();
@@ -18,39 +20,43 @@ export function ClientsShowComponent() {
 
     return (
         <div className={styles.wrapper}>
-            <Grid container justify={"center"}>
-                <Grid item xs={12} md={9}>
-                    <p>{client.firstname} {client.lastname}</p>
-                </Grid>
-
-                <Grid container item direction={"row"} justify={"space-between"} xs={12} md={3}>
-                    <Grid item xs={6}>
-                        <Button variant={"contained"} color={"primary"}
-                                onClick={() => history.push(`/clients/${client?.id}/edit`)}>Modifier</Button>
+            <Paper className={styles.paper}>
+                <Grid container justify={"center"}>
+                    <Grid item xs={12} md={9}>
+                        <p>{client.firstname} {client.lastname}</p>
                     </Grid>
 
-                    <Grid item xs={6}>
-                        <Button variant={"contained"} color={"secondary"}
-                                onClick={() => alert("NOT IMPLEMENTED")}>Supprimer</Button>
+                    <Grid container item direction={"row"} justify={"space-between"} xs={12} md={3}>
+                        <Grid item xs={6}>
+                            <Button variant={"contained"} color={"primary"}
+                                    startIcon={<EditIcon/>}
+                                    onClick={() => history.push(`/clients/${client?.id}/edit`)}>Modifier</Button>
+                        </Grid>
+
+                        <Grid item xs={6}>
+                            <Button variant={"contained"} color={"secondary"}
+                                    startIcon={<DeleteIcon/>}
+                                    onClick={() => alert("NOT IMPLEMENTED")}>Supprimer</Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid item xs={12} md={4}>
+                        <p>Sex: {displayClientSex(client)}</p>
+                    </Grid>
+
+                    <Grid item xs={12} md={4}>
+                        <p>Email: {client.email}</p>
+                    </Grid>
+
+                    <Grid item xs={12} md={4}>
+                        <p>Téléphone: {client.phone}</p>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <p>Rue: {client.street}</p>
                     </Grid>
                 </Grid>
-
-                <Grid item xs={12} md={4}>
-                    <p>Sex: {displayClientSex(client)}</p>
-                </Grid>
-
-                <Grid item xs={12} md={4}>
-                    <p>Email: {client.email}</p>
-                </Grid>
-
-                <Grid item xs={12} md={4}>
-                    <p>Téléphone: {client.phone}</p>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <p>Rue: {client.street}</p>
-                </Grid>
-            </Grid>
+            </Paper>
         </div>
     )
 }
@@ -60,7 +66,10 @@ const useStyles = makeStyles((theme: Theme) =>
         wrapper: {
             padding: theme.spacing(2),
             minWidth: 300,
-        }
+        },
+        paper: {
+            padding: theme.spacing(2),
+        },
     })
 );
 

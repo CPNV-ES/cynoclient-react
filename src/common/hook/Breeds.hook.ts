1680ed368e53a9d4ac914c41cba45a71c4ee19f9
Damien Jakob <damien.jakob@cpnv.ch>
2020-12-09 11:30:26 +0100

    Add single breed access

--- src/common/hook/Breeds.hook.ts
+++ src/common/hook/Breeds.hook.ts
@@ -1,4 +1,4 @@
-import {useQuery} from "react-query";
+import {useQuery, useQueryCache} from "react-query";
 import {List} from "immutable";
 import {Breed} from "../resource/Breed.resource";
 import {BreedsRepository} from "../repository/Breeds.repository";
@@ -6,8 +6,21 @@ import {BreedsRepository} from "../repository/Breeds.repository";
 const BREED_CACHE_KEY = "breeds";
 
 export function useBreeds() {
+    const cache = useQueryCache();
+
     return useQuery<List<Breed>>(
         BREED_CACHE_KEY,
-        () => BreedsRepository.getBreeds()
+        () => BreedsRepository.getBreeds(), {
+            onSuccess: breeds => {
+                breeds.forEach(breed => {
+                    cache.setQueryData([BREED_CACHE_KEY, breed.id], breed);
+                })
+            }
+        }
     )
+}
+
+export function useBreed(id: number) {
+    return useQuery<Breed | null>([BREED_CACHE_KEY, id],
+        () => BreedsRepository.getBreed(id));
 }

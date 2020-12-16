bdd08a69cc6d46b51e5a94b3a4598027a9043742
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-12-16 11:28:54 +0100

    add hook diseases

---
+++ src/common/hook/Diseases.hook.ts
@@ -0,0 +1,26 @@
+import {useQuery, useQueryCache} from "react-query";
+import {List} from "immutable";
+import {Disease} from "../resource/Diseases.ressource";
+import {DiseasesRepository} from "../repository/Diseases.repository";
+
+const DISEASE_CACHE_KEY = "diseases";
+
+export function useDiseases() {
+    const cache = useQueryCache();
+
+    return useQuery<List<Disease>>(
+        DISEASE_CACHE_KEY,
+        () => DiseasesRepository.getDiseases(), {
+            onSuccess: diseases => {
+                diseases.forEach(disease => {
+                    cache.setQueryData([DISEASE_CACHE_KEY, disease.id], disease);
+                })
+            }
+        }
+    )
+}
+
+export function useDisease(id: number) {
+    return useQuery<Disease | null>([DISEASE_CACHE_KEY, id],
+        () => DiseasesRepository.getDisease(id));
+}

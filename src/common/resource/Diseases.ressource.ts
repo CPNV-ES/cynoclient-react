c2778dc8ee4af1e641b8ac95148d6d21d5969c32
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-12-16 11:23:07 +0100

    add repository diseases

--- src/common/resource/Diseases.ressource.ts
+++ src/common/resource/Diseases.ressource.ts
@@ -1,6 +1,6 @@
 import {CommonResource} from "./Common.resource";
 
-export interface Dog extends CommonResource {
+export interface Disease extends CommonResource {
     noun: string,
     description: string,
     symptoms: string,

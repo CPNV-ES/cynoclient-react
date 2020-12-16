2ef2f2e49409b5a9fe97bf2a353dd566eda28200
Alexis Haldy <alexis.haldy@gmail.com>
2020-12-16 11:38:55 +0100

    Finish tables for services

--- src/services/index/ServicesTable.component.tsx
+++ src/services/index/ServicesTable.component.tsx
@@ -7,11 +7,16 @@ import React from "react";
 import TableBody from "@material-ui/core/TableBody";
 import {useServices} from "../../common/hook/Services.hook";
 import {Service} from "../../common/resource/Service.resource";
+import {displayServiceDuration} from "../../common/utils/Service.utils";
 
 export function ServiceRow(service: Service) {
     return (
         <TableRow hover key={service.id}>
             <TableCell>{service.moment}</TableCell>
+            <TableCell>{displayServiceDuration(service)}</TableCell>
+            <TableCell>{service.type}</TableCell>
+            <TableCell>{service.description ? service.description : "-"}</TableCell>
+            <TableCell>{service.street ? service.street : "-"}</TableCell>
         </TableRow>
     );
 }
@@ -25,6 +30,10 @@ export function ServicesTable() {
                 <TableHead>
                     <TableRow>
                         <TableCell>Date</TableCell>
+                        <TableCell>Durée</TableCell>
+                        <TableCell>Type</TableCell>
+                        <TableCell>Description</TableCell>
+                        <TableCell>Adresse</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>

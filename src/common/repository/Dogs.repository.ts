5706bf600d732b92493afa335539e7af26fc78f6
Damien Jakob <damien.jakob@cpnv.ch>
2021-01-06 10:36:34 +0100

    Add dog disease

--- src/common/repository/Dogs.repository.ts
+++ src/common/repository/Dogs.repository.ts
@@ -4,10 +4,10 @@ import {List} from "immutable";
 
 export const DogsRepository = {
     getDogs: (): Promise<List<Dog>> => Axios
-        .get<Dog[]>(`/api/dogs?with[]=client&with[]=breed&with[]=crossbreed`)
+        .get<Dog[]>(`/api/dogs?with[]=client&with[]=breed&with[]=crossbreed&with[]=diseases`)
         .then(res => List(res.data)),
 
     getDog: (id: number): Promise<Dog | null> => Axios
-        .get(`/api/dogs/${id}?with[]=client&with[]=breed&with[]=crossbreed`)
+        .get(`/api/dogs/${id}?with[]=client&with[]=breed&with[]=crossbreed&with[]=diseases`)
         .then(res => res.data),
 }

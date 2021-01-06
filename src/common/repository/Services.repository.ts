7e15146cdca04fd74a23f28ce125351fdb44de33
Alexis Haldy <alexis.haldy@gmail.com>
2021-01-06 10:02:24 +0100

    Create hook and repo

--- src/common/repository/Services.repository.ts
+++ src/common/repository/Services.repository.ts
@@ -6,4 +6,7 @@ export const ServicesRepository = {
     getServices: (): Promise<List<Service>> => Axios
         .get<Service[]>(`/api/services`)
         .then(res => List(res.data)),
+    getService: (id: number): Promise<Service | null> => Axios
+        .get<Service | null>(`/api/services/${id}`)
+        .then(res => res.data),
 }

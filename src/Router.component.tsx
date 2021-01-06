5402937344b233999b04d79159ace830b765645b
Florian Bergmann <florian.bergmann@cpnv.ch>
2021-01-06 12:15:50 +0100

    add route show disease

--- src/Router.component.tsx
+++ src/Router.component.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Redirect, Route, Switch} from "react-router-dom";
 import {ClientsIndexComponent} from "./clients/index/ClientsIndex.component";
-import {DogsIndexComponent} from "./dogs/index/DogsIndex.component";
 import {ClientsShowComponent} from "./clients/show/ClientsShow.component";
+import {DogsIndexComponent} from "./dogs/index/DogsIndex.component";
 import {BreedsIndexComponent} from "./breeds/index/BreedsIndex.component";
 import {DogShowComponent} from "./dogs/show/DogShow.component";
 import {DiseasesIndexComponent} from "./diseases/index/DiseasesIndex.component"
+import {DiseaseShowComponent} from "./diseases/show/DiseasesShow.component";
 
 export function RouterComponent() {
     return (
@@ -24,6 +25,9 @@ export function RouterComponent() {
             </Route>
             <Route path="/dogs">
                 <DogsIndexComponent/>
+            </Route>
+            <Route path="/diseases/:diseaseId/show">
+                <DiseaseShowComponent/>
             </Route>
             <Route path="/diseases">
                 <DiseasesIndexComponent/>

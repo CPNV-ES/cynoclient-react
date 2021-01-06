1d33161f9376e9992499af632efafc6d73c8b60a
Florian Bergmann <florian.bergmann@cpnv.ch>
2021-01-06 11:50:37 +0100

    fix display list diseases

--- src/diseases/index/DiseasesIndex.component.tsx
+++ src/diseases/index/DiseasesIndex.component.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import {DiseasesTable} from "./DiseasesTable.component"
 
 export function DiseasesIndexComponent() {
     return (
         <div>
             <h1>Diseases</h1>
+            <DiseasesTable/>
         </div>
     );
 }
--- src/diseases/index/DiseasesTable.component.tsx
+++ src/diseases/index/DiseasesTable.component.tsx
@@ -2,21 +2,13 @@ import {useDiseases} from "../../common/hook/Diseases.hook";
 import {Table, TableCell, TableContainer, TableHead, TableRow, TableBody} from "@material-ui/core";
 import React from "react";
 import {Disease} from "../../common/resource/Diseases.ressource";
-import {displayBool} from "../../common/utils/Data.utils";
 import {useHistory} from "react-router-dom";
 
 export function DiseaseRow(disease: Disease, onClick: () => void) {
-    const history = useHistory();
-
     return (
         <TableRow hover key={disease.id} onClick={onClick}>
             <TableCell>{disease.noun}</TableCell>
-            <TableCell>{disease.description}</TableCell>
             <TableCell>{disease.symptoms}</TableCell>
-            <TableCell>{disease.preventive}</TableCell>
-            <TableCell>{disease.curative}</TableCell>
-            <TableCell>{displayBool(disease.isVaccinable)}</TableCell>
-            <TableCell>{displayBool(disease.isZoonosis)}</TableCell>
         </TableRow>
     );
 }
@@ -30,18 +22,13 @@ export function DiseasesTable() {
                 <TableHead>
                     <TableRow>
                         <TableCell>Nom</TableCell>
-                        <TableCell>Description</TableCell>
                         <TableCell>Symptomes</TableCell>
-                        <TableCell>Prevention</TableCell>
-                        <TableCell>Soin</TableCell>
-                        <TableCell>Vaccinable ?</TableCell>
-                        <TableCell>Zoonose ?</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {diseases?.map((disease) => DiseaseRow(
                         disease,
-                        () => history.push(`/dogs/${disease.id}`))
+                        () => history.push(`/diseases/${disease.id}`))
                     )}
                 </TableBody>
             </Table>

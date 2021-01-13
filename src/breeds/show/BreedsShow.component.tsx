ae11539ee42e4f392623337cbc507d8d91b85135
Ansermoz Gwenaël <25158748+ansermgw@users.noreply.github.com>
2021-01-13 11:16:43 +0100

    use icons for mesurement table in breed detail and add some labels

--- src/breeds/show/BreedsShow.component.tsx
+++ src/breeds/show/BreedsShow.component.tsx
@@ -13,6 +13,8 @@ import {
     Typography
 } from "@material-ui/core";
 import {makeStyles, Theme} from "@material-ui/core/styles";
+import {BiFemaleSign, BiMaleSign} from "react-icons/bi";
+import {FaWeight, GiBodyHeight} from "react-icons/all";
 
 export function BreedsShowComponent() {
     const styles = useStyles();
@@ -23,11 +25,11 @@ export function BreedsShowComponent() {
         return <Redirect to={"/breeds"}/>
     }
 
-    const generateMeasurementTableRow = (label: string, property: string) => {
+    const generateMeasurementTableRow = (label: JSX.Element, property: string, unit: string) => {
         const untypedBreed = breed as any
 
         const getMeasurementBySex = (sex: string): string => {
-            return untypedBreed[`min_${property}_${sex}`] + " - " + untypedBreed[`max_${property}_${sex}`]
+            return untypedBreed[`min_${property}_${sex}`] + " - " + untypedBreed[`max_${property}_${sex}`] + " " + unit
         }
 
         return (
@@ -40,54 +42,49 @@ export function BreedsShowComponent() {
     }
 
     return (
-        <Grid container direction={"column"} justify={"space-between"} spacing={10}>
-            <Grid item>
-                <Paper>
-                    <Grid item container spacing={2}>
-                        <Grid item container xs={12} sm={2}>
-                            <Grid item>
-                                <img src={breed.picture || ""} className={styles.picture}/>
-                            </Grid>
-                        </Grid>
-                        <Grid container item direction={"column"} xs={12} sm={10}>
-                            <Grid item><Typography variant={"h1"}>{breed.noun}</Typography></Grid>
-                            <Grid item><a href={breed.link} target={"_blank"}>Details</a></Grid>
-                            <Grid item><p>{breed.category?.noun}</p></Grid>
-                        </Grid>
-                        <Grid container item direction={"column"} xs={12} md={6}>
-                            <Grid item><p>{breed.morphotype}</p></Grid>
-                            <Grid item><p>{breed.classification}</p></Grid>
-                            <Grid item><p>{breed.life_expectancy}</p></Grid>
-                        </Grid>
-                        <Grid container item xs={12} md={6}>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell/>
-                                        <TableCell>Male</TableCell>
-                                        <TableCell>Female</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {generateMeasurementTableRow("Taille", "size")}
-                                    {generateMeasurementTableRow("Poids", "weight")}
-                                </TableBody>
-                            </Table>
-                        </Grid>
+        <Paper className={styles.wrapper}>
+            <Grid item container spacing={2}>
+                <Grid item container xs={12} sm={2}>
+                    <Grid item>
+                        <img src={breed.picture || ""} alt={""} className={styles.picture}/>
                     </Grid>
-                </Paper>
+                </Grid>
+                <Grid container item direction={"column"} xs={12} sm={10}>
+                    <Grid item><Typography variant={"h3"}>{breed.noun}</Typography></Grid>
+                    <Grid item><a href={breed.link} target={"_blank"} rel="noreferrer">Details</a></Grid>
+                    <Grid item><p>{breed.category?.noun}</p></Grid>
+                </Grid>
+                <Grid container item direction={"column"} xs={12} md={6}>
+                    <Grid item><p><strong>Morphologie:</strong> {breed.morphotype}</p></Grid>
+                    <Grid item><p><strong>Classification:</strong> {breed.classification}</p></Grid>
+                    <Grid item><p><strong>Age:</strong> {breed.life_expectancy}</p></Grid>
+                </Grid>
+                <Grid container item xs={12} md={6}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell/>
+                                <TableCell><BiMaleSign/></TableCell>
+                                <TableCell><BiFemaleSign/></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {generateMeasurementTableRow(<GiBodyHeight/>, "size", "cm")}
+                            {generateMeasurementTableRow(<FaWeight/>, "weight", "kg")}
+                        </TableBody>
+                    </Table>
+                </Grid>
             </Grid>
-            <Grid item>
-                <Paper>
-                    <p>DOG LIST</p>
-                </Paper>
-            </Grid>
-        </Grid>
+        </Paper>
     )
 }
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        wrapper: {
+            marginTop: theme.spacing(2),
+            padding: theme.spacing(2)
+        },
         picture:
             {
                 width: "100%",
